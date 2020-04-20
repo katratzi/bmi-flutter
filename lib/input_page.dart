a2c9e329546e5ac7c01548cb2dd32225ce41592e
Richard Clifford <richard.clifford@soluis.com>
2020-04-20 17:03:20 +0100

    constants for our colours final keyword in the statelesswidget

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
 
+const bottomContainerHeight = 80.0;
+const activeCardColour = Color(0xff1d1e33);
+const bottomContainerColour = Color(0xffeb1555);
+
 class InputPage extends StatefulWidget {
   @override
   _InputPageState createState() => _InputPageState();
@@ -17,19 +21,25 @@ class _InputPageState extends State<InputPage> {
           Expanded(
             child: Row(
               children: <Widget>[
-                Expanded(child: ReusableCard(colour: Color(0xff1d1e33))),
-                Expanded(child: ReusableCard(colour: Color(0xff1d1e33))),
+                Expanded(child: ReusableCard(colour: activeCardColour)),
+                Expanded(child: ReusableCard(colour: activeCardColour)),
               ],
             ),
           ),
-          Expanded(child: ReusableCard(colour: Colors.blue)),
+          Expanded(child: ReusableCard(colour: activeCardColour)),
           Expanded(
             child: Row(
               children: <Widget>[
-                Expanded(child: ReusableCard(colour: Color(0xff1d1e33))),
-                Expanded(child: ReusableCard(colour: Color(0xff1d1e33))),
+                Expanded(child: ReusableCard(colour: activeCardColour)),
+                Expanded(child: ReusableCard(colour: activeCardColour)),
               ],
             ),
+          ),
+          Container(
+            margin: EdgeInsets.only(top: 10.0),
+            color: bottomContainerColour,
+            height: bottomContainerHeight,
+            width: double.infinity,
           ),
         ],
       ),
@@ -38,7 +48,7 @@ class _InputPageState extends State<InputPage> {
 }
 
 class ReusableCard extends StatelessWidget {
-  Color colour;
+  final Color colour;
 
   ReusableCard({@required this.colour});
 
